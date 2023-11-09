d66984c814dd08ff71f71f77c3a6670ef76a6b18
ApolloOfDelphi <hlzimmerer@gmail.com>
2023-11-08 21:53:44 -0600

    push 2
    Page 731 in textbook,

---
+++ Assets/Scripts/RigidBodySleep.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidBodySleep : MonoBehaviour
+{
+    private int sleepCountdown = 4;
+    private Rigidbody rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        if (sleepCountdown > 0)
+        {
+            rigid.Sleep();
+            sleepCountdown--;
+        }
+    }
+}
